aca33b39996db63698898f1fdef672f5aa5a810f
doin-N <tlatms777@gmail.com>
2024-05-28 16:28:46 +0900

    남은돈계산

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -7,6 +7,7 @@ const Remaining = () => {
  // 배열의 객체 데이터 돈들 다 더하기 total은 누적기 
   const { expenses, budget } = useContext(AppContext);
 
+  //수입과 지출을 합산하여 남은 돈을 계산
   const totalExpenses =
   expenses.reduce((total, itme) => {
     return (total += itme.cost)
